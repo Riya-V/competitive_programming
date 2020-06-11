5992c8f62e6d2cbf9f6d877f416838ba7a6a0143
sourajeetmohanty <m762myfavgun@gmail.com>
2020-06-11 15:29:57 +0530

    added graph

---
+++ garph_theory/connected-components.cpp
@@ -0,0 +1,59 @@
+/*
+emplace_back();
+
+Author: Sourajeet Mohanty
+DEVCPP
+NO endl only '/n'
+*/
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <limits>
+#include <vector>
+#include <bitset>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <time.h>
+
+using namespace std;
+vector<int> arr[100001];
+int vis[100001];
+
+void dfs(int ver)
+{
+	vis[ver]=1;
+	for(int child:arr[ver])
+	{
+		//int child=arr[ver][i];
+		if(vis[child]==0)
+		dfs(child);
+	}
+}
+int main()
+{
+int n,e;
+cin>>n>>e;
+memset(vis,0,sizeof(vis));
+for(int i=0;i<m;i++)
+{
+	int a,b;
+	cin>>a>>b;
+	arr[a].push_back(b);
+	arr[b].push_back(a);
+}
+int cnt=0;
+for(int i=1;i<=n;i++)
+{
+	if(vis[i]==0)
+	{
+		cnt++;
+		dfs(i);
+	}
+	
+}
+cout<<cnt;
+
+return 0;
+}
+
